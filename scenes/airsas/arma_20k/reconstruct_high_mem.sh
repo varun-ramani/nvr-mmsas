a49da8d440c9cc4ec19eed7b85367a60c62cd411
Varun Ramani <varun.ramani@gmail.com>
2024-02-28 13:57:02 -0500

    [rhm_update] fixed?

--- scenes/airsas/arma_20k/reconstruct_high_mem.sh
+++ scenes/airsas/arma_20k/reconstruct_high_mem.sh
@@ -3,11 +3,12 @@
 export SCRIPT_LOCATION="$(dirname $(readlink -e $0))"
 echo "reconstruct_high_mem.sh running from $SCRIPT_LOCATION"
 
-echo $1
-echo $2
+echo "SYSTEM_DATA=$1"
+echo "EXPERIMENT_NAME=$2"
+echo $3
 
 echo "Starting reconstructing using backprojection (the conventional method)"
-python ../../../airsas/reconstruct_from_system_file.py \
+python airsas/reconstruct_from_system_file.py \
   --orig_system_file $1 \
   --output_dir ./reconstructed_scenes  \
   --use_up_to 120 \
@@ -16,8 +17,8 @@ python ../../../airsas/reconstruct_from_system_file.py \
 
 
 echo "Starting pulse deconvolution (reconstruction step 1/2 of our method)"
-python ../../../inr_reconstruction/deconvolve_measurements.py \
-  --inr_config ./pulse_deconvolve.json \
+python inr_reconstruction/deconvolve_measurements.py \
+  --inr_config $SCRIPT_LOCATION/pulse_deconvolve.json \
   --system_data $1 \
   --clear_output_dir \
   --output_dir ./deconvolved_measurements \
@@ -26,12 +27,12 @@ python ../../../inr_reconstruction/deconvolve_measurements.py \
   --number_iterations 1000 \
   --info_every 999 \
   --sparsity 1e-1 \
-  --load_wfm ../../../data/wfm/20khz_bw_lfm.npy \
+  --load_wfm data/wfm/20khz_bw_lfm.npy \
   --phase_loss 1e-4
 
 echo "Starting neural backprojection (reconstruction step 2/2 of our method)"
-python ../../../inr_reconstruction/reconstruct_scene.py \
-  --scene_inr_config ./nbp_config.json \
+python inr_reconstruction/reconstruct_scene.py \
+  --scene_inr_config $SCRIPT_LOCATION/nbp_config.json \
   --fit_folder ./deconvolved_measurements \
   --system_data $1 \
   --output_dir ./nbp_output \
@@ -65,10 +66,10 @@ python ../../../inr_reconstruction/reconstruct_scene.py \
   --max_voxels 15000
 
 echo "Sampling network for scene exporting scene *.mat file"
-python ../../../inr_reconstruction/upsample_network_with_input_args.py \
+python inr_reconstruction/upsample_network_with_input_args.py \
   --exp_name $2 \
   --experiment_dir ./ \
-  --inr_config ./nbp_config.json \
+  --inr_config $SCRIPT_LOCATION/nbp_config.json \
   --output_scene_file_name final_upsampled_scene \
   --output_dir_name reconstructed_scenes \
   --system_data $1 \
